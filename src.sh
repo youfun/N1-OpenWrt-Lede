59f8e41179e2b6c45e67d974ae0918cca90a1c1b
youfun <junezpe@gmail.com>
2023-11-08 21:24:26 +0800

    Update src.sh

--- src.sh
+++ src.sh
@@ -4,6 +4,6 @@ cd openwrt
 echo 'src-git SSRplus https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2' >>feeds.conf.default
-echo 'src/gz openwrt_packages https://dl.openwrt.ai/23.05/packages/aarch64_generic/packages' >>feeds.conf.default
-echo 'src/gz openwrt_luci https://dl.openwrt.ai/23.05/packages/aarch64_generic/luci' >>feeds.conf.default
+# echo 'src/gz openwrt_packages https://dl.openwrt.ai/23.05/packages/aarch64_generic/packages' >>feeds.conf.default
+# echo 'src/gz openwrt_luci https://dl.openwrt.ai/23.05/packages/aarch64_generic/luci' >>feeds.conf.default
 
