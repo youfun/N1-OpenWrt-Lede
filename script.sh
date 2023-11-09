9c7c703b8508d2ea01dbef0cea18f5acb47e8355
youfun <junezpe@gmail.com>
2023-11-09 22:54:07 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -26,3 +26,11 @@ git clone https://github.com/xiaorouji/openwrt-passwall2.git package/passwall2
 #git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/lean/luci-app-smartdns
 #Add luci-app-adguardhome
 #git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/lean/luci-app-adguardhome
+
+#luci-app-filebrowser
+git clone  https://github.com/kenzok8/small-package  package/luci-app-filebrowser
+
+#luci-app-mosdns
+git clone  https://github.com/kenzok8/small-package  package/luci-app-mosdns
+
+
